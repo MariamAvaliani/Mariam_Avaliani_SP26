b8c2022aa09767496b63a3f2d24164ae632dbdb4
Mariam Avaliani <mariamavaliani0217@gmail.com>
2026-06-09 19:50:15 +0400

    Completed session_1

--- Python-Course/Session_1/module_1.py
+++ Python-Course/Session_1/module_1.py
@@ -2,35 +2,73 @@ from typing import List
 
 
 def task_1(array: List[int], target: int) -> List[int]:
-    """
-    Write your code below
-    """
-    pass
+    seen = set()
+
+    for number in array:
+        needed = target - number
+
+        if needed in seen:
+            return [needed, number]
+
+        seen.add(number)
+
+    return []
 
 
 def task_2(number: int) -> int:
-    """
-    Write your code below
-    """
-    pass
+    sign = 1
 
+    if number < 0:
+        sign = -1
+        number = -number
+
+    result = 0
+
+    while number > 0:
+        digit = number % 10
+        result = result * 10 + digit
+        number = number // 10
+
+    return sign * result
 
 def task_3(array: List[int]) -> int:
-    """
-    Write your code below
-    """
-    pass
+    for number in array:
+        index = abs(number) - 1
+
+        if array[index] < 0:
+            return abs(number)
+
+        array[index] = -array[index]
+
+    return -1
 
 
 def task_4(string: str) -> int:
-    """
-    Write your code below
-    """
-    pass
+    values = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000,
+    }
 
+    result = 0
+
+    for i in range(len(string)):
+        if i + 1 < len(string) and values[string[i]] < values[string[i + 1]]:
+            result -= values[string[i]]
+        else:
+            result += values[string[i]]
+
+    return result
 
 def task_5(array: List[int]) -> int:
-    """
-    Write your code below
-    """
-    pass
+    smallest = array[0]
+
+    for num in array:
+        if num < smallest:
+            smallest = num
+
+    return smallest
