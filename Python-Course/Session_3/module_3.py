3f1f6d4af782c6320a40310420a34e76365e4078
Mariam Avaliani <mariamavaliani0217@gmail.com>
2026-06-10 20:42:25 +0400

    completed session_3

--- Python-Course/Session_3/module_3.py
+++ Python-Course/Session_3/module_3.py
@@ -5,24 +5,48 @@ Matrix = List[List[int]]
 
 
 def task_1(exp: int):
-    pass
+    def power(base: int):
+        return base ** exp
+
+    return power
 
 
 def task_2(*args, **kwags):
-    pass
+    for arg in args:
+        print(arg)
+
+    for value in kwags.values():
+        print(value)
 
 
 def helper(func):
-    pass
+    def wrapper(name):
+        print("Hi, friend! What's your name?")
+        func(name)
+        print("See you soon!")
+
+    return wrapper
 
 
 @helper
 def task_3(name: str):
     print(f"Hello! My name is {name}.")
-
+import time
 
 def timer(func):
-    pass
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+
+        result = func(*args, **kwargs)
+
+        end_time = time.time()
+        run_time = end_time - start_time
+
+        print(f"Finished {func.__name__} in {run_time:.4f} secs")
+
+        return result
+
+    return wrapper
 
 
 @timer
@@ -31,8 +55,19 @@ def task_4():
 
 
 def task_5(matrix: Matrix) -> Matrix:
-    pass
+    return [list(row) for row in zip(*matrix)]
 
 
 def task_6(queue: str):
-    pass
+    count = 0
+
+    for ch in queue:
+        if ch == '(':
+            count += 1
+        elif ch == ')':
+            count -= 1
+
+        if count < 0:
+            return False
+
+    return count == 0
